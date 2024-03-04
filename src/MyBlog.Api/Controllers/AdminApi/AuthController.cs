960e07d0a2feba6625577babb541fda8e721c15b
Lương Minh Đức <luongminhducaptech@hotmail.com>
2024-03-04 16:59:11 +0700

    update code 3.5 author and authen

--- src/MyBlog.Api/Controllers/AdminApi/AuthController.cs
+++ src/MyBlog.Api/Controllers/AdminApi/AuthController.cs
@@ -2,13 +2,17 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MyBlog.Api.Extensions;
 using MyBlog.Api.Services;
 using MyBlog.Core.Domain.Identity;
 using MyBlog.Core.Models.Auth;
+using MyBlog.Core.Models.System;
 using MyBlog.Data.SeeWorks.Constants;
 using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
 using System.Security.Claims;
 using System.Text.Json;
+using TeduBlog.Core.SeedWorks.Constants;
 
 namespace MyBlog.Api.Controllers.AdminApi
 {
@@ -19,12 +23,17 @@ namespace MyBlog.Api.Controllers.AdminApi
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly RoleManager<AppRole> _roleManager;
 
-        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
+        public AuthController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager, 
+            ITokenService tokenService,
+            RoleManager<AppRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _roleManager = roleManager;
         }
         [HttpPost]
         public async Task<ActionResult<AuthenticatedResult>> Login(LoginRequest request)
@@ -44,7 +53,7 @@ namespace MyBlog.Api.Controllers.AdminApi
                 return Unauthorized();
             }
             var roles = await _userManager.GetRolesAsync(user);
-            var permission = new List<string>();
+            var permissions = await this.GetPermissionsByUserIdAsync(user.Id.ToString());
 
             var claims = new[]
             {
@@ -76,7 +85,27 @@ namespace MyBlog.Api.Controllers.AdminApi
             var user=await _userManager.FindByIdAsync(userId);
             var roles = await _userManager.GetRolesAsync(user);
             var permissions = new List<string>();
-            var allPermissions=new List<string>();
+            var allPermissions=new List<RoleClaimsDto>();
+            if (roles.Contains(Roles.Admin))
+            {
+                var types = typeof(Permissions).GetTypeInfo().DeclaredNestedTypes;
+                foreach (var type in types)
+                {
+                    allPermissions.GetPermissions(type);
+                }
+                permissions.AddRange(allPermissions.Select(x => x.Value));
+            }
+            else
+            {
+                foreach (var roleName in roles)
+                {
+                    var role = await _roleManager.FindByNameAsync(roleName);
+                    var claims = await _roleManager.GetClaimsAsync(role);
+                    var roleClaimValues=claims.Select(x => x.Value).ToList();
+                    permissions.AddRange(roleClaimValues);
+                }
+            }
+            return permissions.Distinct().ToList();
         }
     }
 }
